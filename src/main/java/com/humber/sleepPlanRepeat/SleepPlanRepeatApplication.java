5c9d5a9b7b9b041aebefc8aa53c641b963b2a7be
lgxnder <n01627582@humber.ca>
2025-03-13 12:07:09 -0400

    +initial project set-up

---
+++ src/main/java/com/humber/sleepPlanRepeat/SleepPlanRepeatApplication.java
@@ -0,0 +1,13 @@
+package com.humber.sleepPlanRepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepPlanRepeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepPlanRepeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/humber/sleepPlanRepeat/SleepPlanRepeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.sleepPlanRepeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepPlanRepeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

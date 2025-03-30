ea0b7e56bf676c144d4d0b57a1d6e3aa7f46590c
yutoiren <149996922+lgxnder@users.noreply.github.com>
2025-03-30 17:44:38 -0400

    updated event repo method

--- src/main/java/com/humber/sleepPlanRepeat/repositories/EventRepository.java
+++ src/main/java/com/humber/sleepPlanRepeat/repositories/EventRepository.java
@@ -2,8 +2,10 @@ package com.humber.sleepPlanRepeat.repositories;
 
 import com.humber.sleepPlanRepeat.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -11,8 +13,10 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findByUserId(Long userId);
-
     List<Event> findByUserIsNull();
     // For use in finding events available to all users.
     // Ex. Christmas.
+
+    @Query("SELECT e FROM Event e WHERE e.user IS NULL AND e.startTime BETWEEN :start AND :end")
+    List<Event> findGlobalEventsByDateRange(LocalDateTime start, LocalDateTime end);
 }

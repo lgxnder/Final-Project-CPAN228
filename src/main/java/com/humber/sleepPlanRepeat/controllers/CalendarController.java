dcc94c29216810278d897d3cac6f8fe8c840f548
yutoiren <149996922+lgxnder@users.noreply.github.com>
2025-03-30 17:02:27 -0400

    updated main controller

--- src/main/java/com/humber/sleepPlanRepeat/controllers/CalendarController.java
+++ src/main/java/com/humber/sleepPlanRepeat/controllers/CalendarController.java
@@ -1,5 +1,6 @@
 package com.humber.sleepPlanRepeat.controllers;
 
+import com.humber.sleepPlanRepeat.models.Event;
 import com.humber.sleepPlanRepeat.repositories.EventRepository;
 import com.humber.sleepPlanRepeat.services.EventService;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,27 +8,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.YearMonth;
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/sleepplanrepeat")
 public class CalendarController {
-    // Inject EventRepository here.
 
-    // Initialize values for use in Thymeleaf templating.
+    private final EventRepository eventRepository;
+
+    public CalendarController(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
     @Value("sleepPlanRepeat")
     private String applicationName;
 
-
     // get mapping /overview
     @GetMapping("/overview")
-    // public String getOverview(Model model)
-    // YearMonth currentMonth = YearMonth.now() // should be March 2025 as of today.
-    // model.addAttribute("currentMonth", currentMonth);
+    public String getOverview(Model model) {
+        YearMonth currentMonth = YearMonth.now(); // should be March 2025 as of today.
+        model.addAttribute("currentMonth", currentMonth);
 
-    // Fetch global events
-    // List<Event> globalEvents = eventRepository.findByUserIsNull();
-    // model.addAttribute("globalEvents", globalEvents);
+        // Fetch global events
+        List<Event> globalEvents = eventRepository.findByUserIsNull();
+        model.addAttribute("globalEvents", globalEvents);
 
-    // TODO: Fetch user-specific events if logged in
-    return "overview";
+        // TODO: Fetch user-specific events if logged in
+        return "overview";
+    }
 }

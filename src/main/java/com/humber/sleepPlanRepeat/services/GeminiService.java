2cfd40b5589c8c7f58bc0d46dc7aadf06c2eb996
Noamakkusu <noahcgo@gmail.com>
2025-04-07 20:50:29 -0400

    geminiservice

--- src/main/java/com/humber/sleepPlanRepeat/services/GeminiService.java
+++ src/main/java/com/humber/sleepPlanRepeat/services/GeminiService.java
@@ -8,6 +8,7 @@ import java.util.Map;
 @Service
 public class GeminiService {
 
+    //injecting api key
     @Value("${gemini.api.key}")
     private String apiKey;
 
@@ -17,7 +18,7 @@ public class GeminiService {
     public GeminiService(WebClient geminiWebClient) {
         this.geminiWebClient = geminiWebClient;
     }
-
+    //User sends question to gemini in order for a response
     public String getPrompt(String userQuestion) {
         Map<String, Object> requestBody = Map.of("contents", new Object[]{
                         Map.of("parts", new Object[]{
@@ -26,6 +27,7 @@ public class GeminiService {
                 }
 
         );
+        //Sends request to Gemini with post method
         String response = geminiWebClient.post()
                 .uri(apiKey)
                 .header("Content-Type", "application/json")

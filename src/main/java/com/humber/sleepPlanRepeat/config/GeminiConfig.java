451043c34bdf11368183e4a1685fbb88e1cc58be
Noamakkusu <noahcgo@gmail.com>
2025-04-07 20:46:28 -0400

    geminiservice

--- src/main/java/com/humber/sleepPlanRepeat/config/GeminiConfig.java
+++ src/main/java/com/humber/sleepPlanRepeat/config/GeminiConfig.java
@@ -1,10 +1,8 @@
 package com.humber.sleepPlanRepeat.config;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import java.util.Map;
 
 @Configuration
